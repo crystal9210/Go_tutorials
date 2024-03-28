68ccb28a7770204c4f7a19c2f7856a21c3aaf1a4
crystal9210 <sy9210sss@yahoo.co.jp>
2024-03-28 09:48:01 +0900

    save

--- ratelimit-template/main.go
+++ ratelimit-template/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"ratelimit-template/middleware"
 
@@ -12,7 +13,7 @@ import (
 func main() {
 	router := gin.Default()
 
-	// セッション管理の設定
+	// すべてのリクエストにセッション管理機能を適用
 	// セッションデータをクッキーに保存するためのストアを作成
 	store := cookie.NewStore([]byte("secret"))
 	router.Use(sessions.Sessions("mysession", store))
@@ -31,9 +32,30 @@ func main() {
 	// →認証、ロギング、CORSポリシーの適用、レートリミティングなどの共通の処理をリクエストの処理チェーンに挿入できる
 	router.Use(middleware.RateLimiterMiddleware())
 
+	// ログイン機能をシミュレートするハンドラー
+	router.GET("/login", func(c *gin.Context) {
+		// セッションの取得
+		session := sessions.Default(c)
+		// セッションにユーザーIDを設定
+		session.Set("userID", "12345")
+		if err := session.Save(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+			return
+		}
+		fmt.Println("Logged in user ID:", session.Get("userID"))
+		c.JSON(http.StatusOK, gin.H{"message": "You are logged in"})
+	})
+
 	// ルートパスのハンドラー
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Welcome to the secure app!"})
+		// セッションからユーザーIDを取得
+		session := sessions.Default(c)
+		userID := session.Get("userID")
+
+		// ターミナルにセッションデータを出力
+		fmt.Printf("Session user ID: %v\n", userID)
+
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome to the secure app!", "userID": userID})
 	})
 
 	router.Run(":8080")
